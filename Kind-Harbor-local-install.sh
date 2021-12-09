8222ba00f58b7fae53b8cca95d4b81a070e80804
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2021-12-10 08:50:20 +1000

    first commit

---
+++ Kind-Harbor-local-install.sh
@@ -0,0 +1,34 @@
+
+#create the kind cluster. Note the use of extra port mappings
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+        authorization-mode: "AlwaysAllow"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+
+
+#deploy contour.Contour is an advanced open source ingress system supported by VMware
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+
+#pathc countour
+kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+
+#add the harbor helm repository
+helm repo add harbor https://helm.goharbor.io
+
+helm install local-harbor harbor/harbor
